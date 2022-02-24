cb408240953054da631be6570fc19a976442d244
Betelihem <Betelihem>
2022-02-23 23:14:02 -0800

    commit tree 6822aa60f26c4ad511c17985bf931da0a4c9ca1d

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const [className, setClassName] = useState("")
+
+  function handleAddToCart() {
+    setClassName (className === "" ? "in-cart": "")
+  }
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>{className === "" ? "Remove Form Cart" : "Add to Cart"}</button>
     </li>
   );
 }
